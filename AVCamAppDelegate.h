bf292e5981561effcc005019e8c57a32da49d92f
codeman <xrickliao@gmail.com>
2017-07-14 03:23:52 +0800

    add some codes

--- AVCamAppDelegate.h
+++ AVCamAppDelegate.h
@@ -5,9 +5,11 @@
 
 
 #import <UIKit/UIKit.h>
+#import "AVCamViewController.h"
 
 @interface AVCamAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (nonatomic) UIWindow *window;
+@property (nonnull) AVCamViewController* avCtrl;
 
 @end
--- AVCamAppDelegate.m
+++ AVCamAppDelegate.m
@@ -7,5 +7,16 @@
 #import "AVCamAppDelegate.h"
 
 @implementation AVCamAppDelegate
+@synthesize window;
+@synthesize avCtrl;
+
+
+-(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    avCtrl = [[AVCamViewController alloc]init];
+    window.rootViewController = avCtrl;
+    
+    return true;
+}
 
 @end
